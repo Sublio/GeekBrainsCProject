adee78d8ce7febd13cff3548f463accf8076ac06
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2021-07-31 01:34:17 +0100

    first htask

---
+++ GeekBrainsCProject/ChessSquareColor.h
@@ -0,0 +1,16 @@
+//
+//  ChessSquareColor.h
+//  GeekBrainsCProject
+//
+//  Created by Denis Mordvinov on 30.07.2021.
+//
+
+#ifndef ChessSquareColor_h
+#define ChessSquareColor_h
+
+#include <stdio.h>
+
+int checkPointColor(char point[]);
+void checkSquaresSameColor(char point1[], char point2[]);
+
+#endif /* ChessSquareColor_h */
