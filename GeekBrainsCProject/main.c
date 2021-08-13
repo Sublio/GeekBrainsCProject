6cb8396f90dc6a48ac11c33943dc9eb267702c73
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2021-08-13 20:02:30 +0100

    dynamic data

--- GeekBrainsCProject/main.c
+++ GeekBrainsCProject/main.c
@@ -12,147 +12,179 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void pathFinder()
-{
-    
-    //    Map
-    //    3 3
-    //    1 1 1
-    //    0 1 0
-    //    0 1 0
-    
-    int N = 0, M = 0;
-    
-    printf("Input parameters for NxM map\n");
-    printf("format N-M, like 3-3 : \n");
-    scanf("%d-%d", &N, &M);
-    
-    printf("1 - we can move\n");
-    printf("0 - we forbid to  move\n");
-    printf("\n");
-    
-    int roadBoard[N][M];
-    int value, i, j;
-    
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            printf("Input value 1 or 0 for [%d]x[%d] : \n", i, j);
-            scanf("%d", &value);
-            if ((value > 1) || (value < 0))
-                roadBoard[i][j] = 1;
-            else
-                roadBoard[i][j] = value;
+#define max 100
+#define MAX 50
+
+void insert(void);
+void delete(void);
+void display(void);
+int queue_array[MAX];
+int rear = -1;
+int front = -1;
+
+
+
+int stack[max], top = -1, i, x;
+
+void queueManager(void){
+    int choice;
+    while(1){
+        printf("1.Insert element to queue \n");
+        printf("2.Delete element from queue \n");
+        printf("3.Display all elements of queue \n");
+        printf("4.Quit \n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(1);
+                break;
+            default:
+                printf("Wrong number selected\n");
+                break;
         }
     }
-    
-    printf("\n");
-    printf("Entered values : \n");
-    
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++)
-            printf("%d  ", roadBoard[i][j]);
-        printf("\n");
-    }
-    
-    roadBoard[0][0] = 1;
-    
-    for (i = 1; i < N - 1; i ++) {
-        if (roadBoard[i][0] == 0)
-            roadBoard[i+1][0] = 0;
-    }
-    for (j = 1; j < M - 1; j ++) {
-        if (roadBoard[0][j] == 0)
-            roadBoard[0][j+1] = 0;
-    }
-    
-    printf("\n");
-    printf("Map matrix : \n");
-    
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++)
-            printf("%d  ", roadBoard[i][j]);
-        printf("\n");
-    }
-    
-    printf("\n");
-    
-    for (i = 1; i < N; i++) {
-        for (j = 1; j < M; j++)
-            if (roadBoard[i][j] != 0)
-                roadBoard[i][j] = roadBoard[i][j-1] + roadBoard[i-1][j];
-    }
-    
-    
-    printf("\n");
-    printf("Steps count matrix : \n");
-    
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++)
-            printf("%d     ", roadBoard[i][j]);
-        printf("\n");
-        printf("\n");
-    }
-    
-    printf("\n");
-    printf("\n");
-    
 }
 
-int max(int a, int b);
-
-int lcs( char *X, char *Y, int m, int n )
-{
-    int L[m+1][n+1];
-    int i, j;
-    
-    for (i=0; i<=m; i++)
+void insert(void){
+    int add_item;
+    if (rear == MAX - 1)
+        printf("Queue Overflow \n");
+    else
     {
-        for (j=0; j<=n; j++)
-        {
-            if (i == 0 || j == 0)
-                L[i][j] = 0;
-            
-            else if (X[i-1] == Y[j-1])
-                L[i][j] = L[i-1][j-1] + 1;
-            
-            else
-                L[i][j] = max(L[i-1][j], L[i][j-1]);
-        }
+        if (front == - 1)
+        /*If queue is initially empty */
+            front = 0;
+        printf("Inset the element in queue : ");
+        scanf("%d", &add_item);
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+    }
+}
+
+
+void delete(void)
+{
+    if (front == - 1 || front > rear)
+    {
+        printf("Queue Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from queue is : %d\n", queue_array[front]);
+        front = front + 1;
+    }
+}
+
+
+void display(void)
+{
+    int i;
+    if (front == - 1)
+        printf("Queue is empty \n");
+    else
+    {
+        printf("Queue is : \n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+    }
+}
+
+void push(int x){
+    ++top;
+    stack[top] = x;
+}
+
+int pop(void){
+    return stack[top];
+}
+
+void stackConvertion(void){
+    int num, total = 0, item;
+    printf("Please enter the number: ");
+    scanf("%d", &num);
+    
+    while(num>0){
+        total = num%2;
+        push(total);
+        num/=2;
     }
     
+    for (i = top; top>-1; top --){
+        item = pop();
+        printf("%d", item);
+    }
     
-    return L[m][n];
 }
 
-int max(int a, int b)
-{
-    return (a > b)? a : b;
+void bracketSequence(void){
+    int c=0,count1=0,count2=0;
+    char s[1000];
+    printf("Enter bracket sequence: ");
+    scanf("%s",s);
+    while(s[c] != '\0')
+    {
+        if(s[c] == '"')
+        {
+            ++count2;
+        }
+        if(s[c] == ')')
+        {
+            --count1;
+        }
+        if (count1 != -1)
+        {
+            if(s[c]== '(')
+            {
+                ++count1;
+            }
+        }
+        
+        ++c;
+    }
+    //printf("%d\n",count1);
+    //printf("%d\n",count2);
+    if(count1 == 0 && count2%2 == 0)
+    {
+        printf("Sequence is valid!\n");
+    }
+    else
+    {
+        printf("Sequence is not valid!\n");
+    }
 }
-
 
 
 void showMenu(void) {
     int select;
     printf("Выберите алгоритм:\n");
-    printf("[1] Вычислить длину подпоследовательности;\n");
-    printf("[2] Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и нахождение количество маршрутов;\n");
+    printf("[1] Реализовать перевод из десятичной в двоичную систему счисления с использованием стека;\n");
+    printf("[2] Написать программу, которая определяет, является ли введенная скобочная последовательность правильной;\n");
+    printf("[3] Реализовать очередь;\n");
     printf("[0] выход \n");
     
     scanf("%d", &select);
     
-    char X[] = "AGGTAB";
-    char Y[] = "GXTXAYB";
-     
-    int m = strlen(X);
-    int n = strlen(Y);
-    
-    
     switch (select) {
         case 1:
-            printf("Длина подпоследовательности = %d\n", lcs( X, Y, m, n ) );
+            stackConvertion();
             break;
         case 2:
-            pathFinder();
+            bracketSequence();
+            break;
+        case 3:
+            queueManager();
             break;
         case 0:
         default:
